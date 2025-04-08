158445840d629b9d3e32c178255e8549e49931e6
TrungDavidNguyen <davidtrung03@gmail.com>
2025-04-08 18:00:21 +0200

    imagenet a

--- measure_acc_example.py
+++ measure_acc_example.py
@@ -25,25 +25,25 @@ def main(model_name):
     model_row = df_imagenet[df_imagenet['Model'] == model_name]
     id_accuracy = model_row['imagenet1k'].values[0]
     print(model_name," id accuracy", id_accuracy)
-    ood_path = os.path.join(os.getcwd(), "imagenetv2-matched-frequency-format-val")
+    ood_path = os.path.join(os.getcwd(), "imagenet-a")
     ood_accuracy = measure_accuracy(model, ood_path, transform)
     print(model_name," ood accuracy", ood_accuracy)
-    # values for imagenet v2
-    intercept = -0.4813069457734013
-    slope =  0.9113725552359271
+    # values for imagenet a
+    intercept = -6.291670522669618
+    slope = 3.051638725976987
     eff_robustness = effective_robustness(id_accuracy, ood_accuracy, intercept, slope)
     print(model_name, "eff robust", eff_robustness)
-    df = pd.DataFrame(columns=['Model', 'eff.Robustness', 'imagenet1k', 'imagenetv2-matched-frequency'])
+    df = pd.DataFrame(columns=['Model', 'eff.Robustness', 'imagenet1k', 'imagenet-a'])
     df.loc[len(df)] = [model_name,eff_robustness,id_accuracy,ood_accuracy]
 
     # Save to CSV
-    csv_filename = 'results/results-imagenetv2-matched-frequency.csv'
+    csv_filename = 'results/results-imagenet-a.csv'
     file_exists = os.path.isfile(csv_filename)
     df.to_csv(csv_filename, mode='a', index=False, header=not file_exists)
 
 
 if __name__ == '__main__':
-    num = int(sys.argv[1])
+    num =int(sys.argv[1])
     models_list = ['ResNet50','AlexNet','Densenet121', 'Densenet161', 'Densenet169', 'Densenet201',
                'GoogleNet', 'ResNet101', 'ResNet152', 'ResNet18', 'ResNet34',
                'ShuffleNetV2x05', 'ShuffleNetV2x10', 'Squeezenet1_0', 'Squeezenet1_1',
