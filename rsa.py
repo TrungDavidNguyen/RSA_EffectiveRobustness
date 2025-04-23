ffbf835a25301dcfa17dab6ac869c6a29d02839a
TrungDavidNguyen <davidtrung03@gmail.com>
2025-04-23 11:00:50 +0200

    rsa fix

--- rsa.py
+++ rsa.py
@@ -47,8 +47,7 @@ if __name__ == '__main__':
                    'efficientnet_b2', 'efficientnet_b3', 'efficientnet_b4', 'efficientnet_b5',
                    'mnasnet05', 'mnasnet10', 'mobilenet_v2',
                    'mobilenet_v3_large', 'mobilenet_v3_small']"""
-    models_list = ['inception_v3', 'inception_resnet_v2', 'xception',
-                  'nasnetalarge', 'pnasnet5large', 'tf_efficientnet_b2_ns','tf_efficientnet_b4_ns'
+    models_list = ['nasnetalarge', 'pnasnet5large', 'tf_efficientnet_b2_ns','tf_efficientnet_b4_ns',
                   'resnext50_32x4d', 'resnext101_32x8d',
                   'vit_base_patch16_224', 'vit_large_patch16_224', 'vit_huge_patch14_224_in21k',
                   'deit_base_patch16_224', 'swin_base_patch4_window7_224',
