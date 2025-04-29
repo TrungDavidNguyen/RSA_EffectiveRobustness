9c36cdd25422a84012039fb4710d4639ccf53622
TrungDavidNguyen <davidtrung03@gmail.com>
2025-04-29 11:08:29 +0200

    encoding update models

--- encoding.py
+++ encoding.py
@@ -60,15 +60,8 @@ if __name__ == '__main__':
                        'efficientnet_b2', 'efficientnet_b3', 'efficientnet_b4', 'efficientnet_b5',
                        'mnasnet05', 'mnasnet10', 'mobilenet_v2',
                        'mobilenet_v3_large', 'mobilenet_v3_small']"""
-    models_list = ['inception_v3', 'inception_resnet_v2', 'xception',
-                  'tf_efficientnet_b2_ns','tf_efficientnet_b4_ns'
-                  'resnext50_32x4d', 'resnext101_32x8d',
-                  'vit_base_patch16_224', 'vit_large_patch16_224',
-                  'deit_base_patch16_224', 'swin_base_patch4_window7_224',
-                  'mixer_b16_224', 'nfnet_l0', 'dm_nfnet_f0', 'regnety_032', 'regnety_080',
-                  'coat_lite_mini','seresnet50',
-                  'gluon_resnet50_v1c', 'gluon_resnext101_64x4d',
-                  'wide_resnet50_2']
+    models_list = ['tf_efficientnet_b4_ns',
+                  'resnext50_32x4d', 'vit_large_patch16_224']
     model_name = models_list[num]
     encoding(model_name, "Timm", "V4")
     encoding(model_name, "Timm", "IT")
