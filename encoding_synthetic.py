b2adc5f71ba8375cc3fb118346cc163754ff25b7
TrungDavidNguyen <davidtrung03@gmail.com>
2025-05-06 00:50:19 +0200

    acc

--- encoding_synthetic.py
+++ encoding_synthetic.py
@@ -9,6 +9,7 @@ from net2brain.utils.download_datasets import DatasetNSD_872
 
 
 def encoding(model_name, netset, roi_name):
+    dataset = "encoding_synthetic"
     device = "cuda" if torch.cuda.is_available() else "cpu"
     current_dir = os.getcwd()
     feat_path = f"{model_name}_Feat"
@@ -21,17 +22,17 @@ def encoding(model_name, netset, roi_name):
     features = fx.extract(data_path=stimuli_path, save_path=feat_path, consolidate_per_layer=False,  layers_to_extract=layers_to_extract)
     R_sum = 0
     for subj in range(1, 9):
-        roi_path = os.path.join(current_dir, f"fmri/{roi_name}/{roi_name}_fmri_subj{subj}")
-        df = RidgeCV_Encoding(features, roi_path, model_name, np.logspace(-3, 3, 10), save_path=f"encoding/{roi_name}/encoding_{roi_name}_subj{subj}")
+        roi_path = os.path.join(current_dir, f"fmri_synthetic/{roi_name}/{roi_name}_fmri_subj{subj}")
+        df = RidgeCV_Encoding(features, roi_path, model_name, np.logspace(-3, 3, 10), save_path=f"{dataset}/{roi_name}/encoding_{roi_name}_subj{subj}")
         df = df[['ROI', 'Layer', 'Model', 'R']]
         df = df.loc[[df['R'].idxmax()]]
         R_sum += df.loc[df.index[0], "R"]
-        csv_filename = f'encoding/{roi_name}/encoding_{roi_name}_subj{subj}/results-encoding-{roi_name}.csv'
+        csv_filename = f'{dataset}/{roi_name}/encoding_{roi_name}_subj{subj}/results-encoding-{roi_name}.csv'
         file_exists = os.path.isfile(csv_filename)
         df.to_csv(csv_filename, mode='a', index=False, header=not file_exists)
     R_mean = R_sum/8
 
-    csv_filename = 'results/encoding.csv'
+    csv_filename = f'results/{dataset}.csv'
 
     if os.path.exists(csv_filename):
         df = pd.read_csv(csv_filename)
@@ -52,9 +53,14 @@ def encoding(model_name, netset, roi_name):
 
 if __name__ == '__main__':
     num = int(sys.argv[1])
-    models_list = ['VGG11', 'VGG11_bn', 'VGG13', 'VGG13_bn', 'VGG16',
-                   'VGG16_bn', 'VGG19', 'VGG19_bn']
-    """    models_list = ['inception_resnet_v2', 'vit_large_patch16_224','convit_small']"""
+    models_list = ['ResNet50', 'AlexNet', 'Densenet121', 'Densenet161', 'Densenet169', 'Densenet201',
+                   'GoogleNet', 'ResNet101', 'ResNet152', 'ResNet18', 'ResNet34',
+                   'ShuffleNetV2x05', 'ShuffleNetV2x10', 'Squeezenet1_0', 'Squeezenet1_1',
+                   'VGG11', 'VGG11_bn', 'VGG13', 'VGG13_bn', 'VGG16',
+                   'VGG16_bn', 'VGG19', 'VGG19_bn', 'efficientnet_b0', 'efficientnet_b1',
+                   'efficientnet_b2', 'efficientnet_b3', 'efficientnet_b4', 'efficientnet_b5',
+                   'mnasnet05', 'mnasnet10', 'mobilenet_v2',
+                   'mobilenet_v3_large', 'mobilenet_v3_small']
 
     model_name = models_list[num]
     encoding(model_name, "Standard", "V4")
