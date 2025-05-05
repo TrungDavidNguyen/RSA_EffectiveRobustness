634e4771a41da7037e7302575d89b0dbfa5c84f0
TrungDavidNguyen <davidtrung03@gmail.com>
2025-05-05 12:57:09 +0200

    rsa

--- rsa.py
+++ rsa.py
@@ -51,15 +51,14 @@ def main(model_name, netset, roi_name, device="cuda" if torch.cuda.is_available(
 
 if __name__ == '__main__':
     num = int(sys.argv[1])
-    """    models_list = ['ResNet50', 'AlexNet', 'Densenet121', 'Densenet161', 'Densenet169', 'Densenet201',
-                       'GoogleNet', 'ResNet101', 'ResNet152', 'ResNet18', 'ResNet34',
-                       'ShuffleNetV2x05', 'ShuffleNetV2x10', 'Squeezenet1_0', 'Squeezenet1_1',
-                       'VGG11', 'VGG11_bn', 'VGG13', 'VGG13_bn', 'VGG16',
-                       'VGG16_bn', 'VGG19', 'VGG19_bn', 'efficientnet_b0', 'efficientnet_b1',
-                       'efficientnet_b2', 'efficientnet_b3', 'efficientnet_b4', 'efficientnet_b5',
-                       'mnasnet05', 'mnasnet10', 'mobilenet_v2',
-                       'mobilenet_v3_large', 'mobilenet_v3_small']"""
-    models_list = ['vit_large_patch16_224','mixer_b16_224']
+    models_list = ['ResNet50', 'AlexNet', 'Densenet121', 'Densenet161', 'Densenet169', 'Densenet201',
+                   'GoogleNet', 'ResNet101', 'ResNet152', 'ResNet18', 'ResNet34',
+                   'ShuffleNetV2x05', 'ShuffleNetV2x10', 'Squeezenet1_0', 'Squeezenet1_1',
+                   'VGG11', 'VGG11_bn', 'VGG13', 'VGG13_bn', 'VGG16',
+                   'VGG16_bn', 'VGG19', 'VGG19_bn', 'efficientnet_b0', 'efficientnet_b1',
+                   'efficientnet_b2', 'efficientnet_b3', 'efficientnet_b4', 'efficientnet_b5',
+                   'mnasnet05', 'mnasnet10', 'mobilenet_v2',
+                   'mobilenet_v3_large', 'mobilenet_v3_small']
     model_name = models_list[num]
     main(model_name, "Timm", "V4")
     main(model_name, "Timm", "IT")
