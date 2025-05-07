1be93aff8ab42b88e508f06fdcd72c209a6121a5
GoetheCoder <davidtrung03@gmail.com>
2025-05-07 12:15:50 +0200

    encoding

--- encoding.py
+++ encoding.py
@@ -52,7 +52,7 @@ def encoding(model_name, netset, roi_name):
 
 if __name__ == '__main__':
     num = int(sys.argv[1])
-    models_list = ['VGG13_bn', 'VGG16']
+    models_list = ['VGG16']
     """    models_list = ['inception_resnet_v2', 'vit_large_patch16_224','convit_small']"""
 
     model_name = models_list[num]
