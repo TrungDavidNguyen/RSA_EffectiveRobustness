8fb68789fda63e19cac81ddc57d5c322b7b8607c
GoetheCoder <davidtrung03@gmail.com>
2025-05-17 14:44:39 +0200

    rsa

--- rsa.py
+++ rsa.py
@@ -51,17 +51,11 @@ def main(model_name, netset, roi_name, device="cuda" if torch.cuda.is_available(
 
 if __name__ == '__main__':
     num = int(sys.argv[1])
-    models_list = ['swin_base_patch4_window7_224','seresnet50','gluon_resnet50_v1c', 'gluon_resnext101_64x4d','wide_resnet50_2',
-                   'convit_small',
-                   'ResNet50', 'AlexNet', 'Densenet121', 'Densenet161', 'Densenet169',
-                   'Densenet201', 'GoogleNet', 'ResNet101', 'ResNet152', 'ResNet18',
-                   'ResNet34', 'ShuffleNetV2x05', 'ShuffleNetV2x10', 'Squeezenet1_0', 'Squeezenet1_1',
-                   'VGG11', 'VGG11_bn', 'VGG13', 'VGG13_bn', 'VGG16',
-                   'VGG16_bn', 'VGG19', 'VGG19_bn', 'efficientnet_b0', 'efficientnet_b1',
-                   'efficientnet_b2', 'efficientnet_b3', 'efficientnet_b4', 'efficientnet_b5', 'mnasnet05',
-                   'mnasnet10', 'mobilenet_v2', 'mobilenet_v3_large', 'mobilenet_v3_small']
+    models_list = ['regnety_032', 'ResNet18', 'Squeezenet1_1', 'VGG11', 'VGG11_bn',
+                   'VGG13_bn', 'VGG16', 'VGG16_bn', 'VGG19', 'VGG19_bn',
+                   'efficientnet_b2', 'mnasnet10', 'mobilenet_v2', 'mobilenet_v3_large', 'mobilenet_v3_small']
     model_name = models_list[num]
-    if num<=5:
+    if num<=0:
         main(model_name, "Timm", "V1")
         main(model_name, "Timm", "V2")
     else:
