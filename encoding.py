ed989ce91d05d87ced708de752a8d121b44b400d
TrungDavidNguyen <davidtrung03@gmail.com>
2025-06-06 23:10:34 +0200

    encoding cleanup

--- encoding.py
+++ encoding.py
@@ -5,38 +5,28 @@ import pandas as pd
 import numpy as np
 from utils.feature_extraction import FeatureExtractor
 from utils.ridge_regression import RidgeCV_Encoding
-from net2brain.utils.download_datasets import DatasetNSD_872
 from timm.data import resolve_data_config
 from timm.data.transforms_factory import create_transform
 from timm import create_model
 import torchextractor as tx
 
 
-def encoding(model_name, netset, roi_name, features=None):
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+def save_encoding_results(fmri_dataset, roi_name, save_folder, num_subjects):
     current_dir = os.getcwd()
-    feat_path = f"{model_name}_Feat"
-    dataset_path = DatasetNSD_872.load_dataset()
-    stimuli_path = dataset_path["NSD_872_images"]
 
-    # Extract features
-    if features is None:
-        fx = FeatureExtractor(model=model_name, netset=netset, device=device)
-        layers_to_extract = fx.get_all_layers()
-        features = fx.extract(data_path=stimuli_path, save_path=feat_path, consolidate_per_layer=False,  layers_to_extract=layers_to_extract)
     R_sum = 0
-    for subj in range(1, 9):
-        roi_path = os.path.join(current_dir, f"fmri/{roi_name}/{roi_name}_fmri_subj{subj}")
-        df = RidgeCV_Encoding(features, roi_path, model_name, np.logspace(-3, 3, 10), save_path=f"encoding/{roi_name}/encoding_{roi_name}_subj{subj}")
+    for subj in range(1, num_subjects+1):
+        roi_path = os.path.join(current_dir, f"{fmri_dataset}/{roi_name}/{roi_name}_fmri_subj{subj}")
+        df = RidgeCV_Encoding(features, roi_path, model_name, np.logspace(-3, 3, 10), save_path=f"{save_folder}/{roi_name}/encoding_{roi_name}_subj{subj}")
         df = df[['ROI', 'Layer', 'Model', 'R']]
         df = df.loc[[df['R'].idxmax()]]
         R_sum += df.loc[df.index[0], "R"]
-        csv_filename = f'encoding/{roi_name}/encoding_{roi_name}_subj{subj}/results-encoding-{roi_name}.csv'
+        csv_filename = f'{save_folder}/{roi_name}/encoding_{roi_name}_subj{subj}/results-encoding-{roi_name}.csv'
         file_exists = os.path.isfile(csv_filename)
         df.to_csv(csv_filename, mode='a', index=False, header=not file_exists)
-    R_mean = R_sum/8
+    R_mean = R_sum/num_subjects
 
-    csv_filename = 'results/encoding.csv'
+    csv_filename = f'results/{save_folder}.csv'
 
     if os.path.exists(csv_filename):
         df = pd.read_csv(csv_filename)
@@ -53,52 +43,28 @@ def encoding(model_name, netset, roi_name, features=None):
         df = pd.DataFrame({"Model": [model_name], f"R_{roi_name}": [R_mean]})
 
     df.to_csv(csv_filename, index=False)
+
+
+def encoding(model_name, netset, roi_name, stimuli_path, fmri_dataset, save_folder, num_subjects, features=None, device="cuda" if torch.cuda.is_available() else "cpu"):
+    # Extract features
+    if features is None:
+        fx = FeatureExtractor(model=model_name, netset=netset, device=device)
+        layers_to_extract = fx.get_all_layers()
+        features = fx.extract(data_path=stimuli_path, consolidate_per_layer=False,  layers_to_extract=layers_to_extract)
+    save_encoding_results(fmri_dataset, roi_name, save_folder, num_subjects)
+
     return features
 
 
-def encoding_custom(model_name, roi_name, features=None):
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    current_dir = os.getcwd()
-    feat_path = f"{model_name}_Feat"
-    dataset_path = DatasetNSD_872.load_dataset()
-    stimuli_path = dataset_path["NSD_872_images"]
-
+def encoding_custom(model_name, roi_name, stimuli_path, fmri_dataset, save_folder, num_subjects, features=None, device="cuda" if torch.cuda.is_available() else "cpu"):
     # Extract features
     if features is None:
         model = create_model(model_name, pretrained=True)
         fx = FeatureExtractor(model=model, device=device, preprocessor=my_preprocessor, feature_cleaner=my_cleaner,
                               extraction_function=my_extractor)
         layers_to_extract = fx.get_all_layers()
-        features = fx.extract(data_path=stimuli_path, save_path=feat_path, consolidate_per_layer=False,  layers_to_extract=layers_to_extract)
-    R_sum = 0
-    for subj in range(1, 9):
-        roi_path = os.path.join(current_dir, f"fmri/{roi_name}/{roi_name}_fmri_subj{subj}")
-        df = RidgeCV_Encoding(features, roi_path, model_name, np.logspace(-3, 3, 10), save_path=f"encoding/{roi_name}/encoding_{roi_name}_subj{subj}")
-        df = df[['ROI', 'Layer', 'Model', 'R']]
-        df = df.loc[[df['R'].idxmax()]]
-        R_sum += df.loc[df.index[0], "R"]
-        csv_filename = f'encoding/{roi_name}/encoding_{roi_name}_subj{subj}/results-encoding-{roi_name}.csv'
-        file_exists = os.path.isfile(csv_filename)
-        df.to_csv(csv_filename, mode='a', index=False, header=not file_exists)
-    R_mean = R_sum/8
-
-    csv_filename = 'results/encoding.csv'
-
-    if os.path.exists(csv_filename):
-        df = pd.read_csv(csv_filename)
-        if model_name in df['Model'].values:
-            if f"R_{roi_name}" not in df.columns:
-                df[f"R_{roi_name}"] = None
-            df.loc[df['Model'] == model_name, f"R_{roi_name}"] = R_mean
-        else:
-            new_row = pd.Series({col: None for col in df.columns})
-            new_row['Model'] = model_name
-            new_row[f"R_{roi_name}"] = R_mean
-            df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
-    else:
-        df = pd.DataFrame({"Model": [model_name], f"R_{roi_name}": [R_mean]})
-
-    df.to_csv(csv_filename, index=False)
+        features = fx.extract(data_path=stimuli_path, consolidate_per_layer=False,  layers_to_extract=layers_to_extract)
+    save_encoding_results(fmri_dataset, roi_name, save_folder, num_subjects)
     return features
 
 
@@ -139,11 +105,15 @@ def my_cleaner(features):
 
 if __name__ == '__main__':
     num = int(sys.argv[1])
-    models_list = ['vit_base_patch16_clip_224.openai', 'efficientnet_b3.ra2_in1k', 'vit_base_patch16_224.dino', 'beit_base_patch16_224.in22k_ft_in22k_in1k',
-                   'gmlp_s16_224.ra3_in1k', 'vit_base_patch16_224.mae', 'convnext_base.fb_in22k_ft_in1k']
+    models_list = ['efficientnet_b3.ra2_in1k', 'beit_base_patch16_224.in22k_ft_in22k_in1k',
+                   'gmlp_s16_224.ra3_in1k', 'convnext_base.fb_in22k_ft_in1k']
     model_name = models_list[num]
 
-    features = encoding_custom(model_name,  "V1")
-    encoding_custom(model_name, "V2", features)
-    encoding_custom(model_name, "V4", features)
-    encoding_custom(model_name, "IT", features)
+    stimuli_path = os.path.join(os.getcwd(), "NSD Dataset", "NSD_872_images")
+    fmri_dataset = "fmri"
+    save_folder = "encoding"
+
+    features = encoding_custom(model_name,  "V1", stimuli_path, fmri_dataset, save_folder, 8)
+    encoding_custom(model_name, "V2", stimuli_path, fmri_dataset, save_folder, 8, features)
+    encoding_custom(model_name, "V4", stimuli_path, fmri_dataset, save_folder, 8, features)
+    encoding_custom(model_name, "IT", stimuli_path, fmri_dataset, save_folder, 8, features)
---
+++ encoding_illusion.py
@@ -0,0 +1,160 @@
+import os
+import torch
+import sys
+import pandas as pd
+import numpy as np
+from utils.feature_extraction import FeatureExtractor
+from utils.ridge_regression import RidgeCV_Encoding
+from timm.data import resolve_data_config
+from timm.data.transforms_factory import create_transform
+from timm import create_model
+import torchextractor as tx
+
+
+def save_encoding_results(fmri_dataset, roi_name, save_folder, num_subjects):
+    current_dir = os.getcwd()
+
+    R_sum = 0
+    for subj in [1, 2, 3, 5, 6, 7]:
+        roi_path = os.path.join(current_dir, f"{fmri_dataset}/{roi_name}/{roi_name}_fmri_subj{subj}")
+        df = RidgeCV_Encoding(features, roi_path, model_name, np.logspace(-3, 3, 10), save_path=f"{save_folder}/{roi_name}/encoding_{roi_name}_subj{subj}")
+        df = df[['ROI', 'Layer', 'Model', 'R']]
+        df = df.loc[[df['R'].idxmax()]]
+        R_sum += df.loc[df.index[0], "R"]
+        csv_filename = f'{save_folder}/{roi_name}/encoding_{roi_name}_subj{subj}/results-encoding-{roi_name}.csv'
+        file_exists = os.path.isfile(csv_filename)
+        df.to_csv(csv_filename, mode='a', index=False, header=not file_exists)
+    R_mean = R_sum/num_subjects
+
+    csv_filename = f'results/{save_folder}.csv'
+
+    if os.path.exists(csv_filename):
+        df = pd.read_csv(csv_filename)
+        if model_name in df['Model'].values:
+            if f"R_{roi_name}" not in df.columns:
+                df[f"R_{roi_name}"] = None
+            df.loc[df['Model'] == model_name, f"R_{roi_name}"] = R_mean
+        else:
+            new_row = pd.Series({col: None for col in df.columns})
+            new_row['Model'] = model_name
+            new_row[f"R_{roi_name}"] = R_mean
+            df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+    else:
+        df = pd.DataFrame({"Model": [model_name], f"R_{roi_name}": [R_mean]})
+
+    df.to_csv(csv_filename, index=False)
+
+
+def encoding(model_name, netset, roi_name, stimuli_path, fmri_dataset, save_folder, num_subjects, features=None, device="cuda" if torch.cuda.is_available() else "cpu"):
+    # Extract features
+    if features is None:
+        fx = FeatureExtractor(model=model_name, netset=netset, device=device)
+        layers_to_extract = fx.get_all_layers()
+        features = fx.extract(data_path=stimuli_path, consolidate_per_layer=False,  layers_to_extract=layers_to_extract)
+    save_encoding_results(fmri_dataset, roi_name, save_folder, num_subjects)
+
+    return features
+
+
+def encoding_custom(model_name, roi_name, stimuli_path, fmri_dataset, save_folder, num_subjects, features=None, device="cuda" if torch.cuda.is_available() else "cpu"):
+    # Extract features
+    if features is None:
+        model = create_model(model_name, pretrained=True)
+        fx = FeatureExtractor(model=model, device=device, preprocessor=my_preprocessor, feature_cleaner=my_cleaner,
+                              extraction_function=my_extractor)
+        layers_to_extract = fx.get_all_layers()
+        features = fx.extract(data_path=stimuli_path, consolidate_per_layer=False,  layers_to_extract=layers_to_extract)
+    save_encoding_results(fmri_dataset, roi_name, save_folder, num_subjects)
+    return features
+
+
+def my_preprocessor(image, model_name, device):
+    """
+    Args:
+        image (Union[Image.Image, List[Image.Image]]): A PIL Image or a list of PIL Images.
+        model_name (str): The name of the model, used to determine specific preprocessing if necessary.
+        device (str): The device to which the tensor should be transferred ('cuda' for GPU, 'cpu' for CPU).
+
+    Returns:
+        Union[torch.Tensor, List[torch.Tensor]]: The preprocessed image(s) as PyTorch tensor(s).
+    """
+
+    model = create_model(model_name, pretrained=True)
+    config = resolve_data_config({}, model=model)
+    transform = create_transform(**config)
+    img_tensor = transform(image).unsqueeze(0)
+    if device == 'cuda':
+        img_tensor = img_tensor.cuda()
+
+    return img_tensor
+
+
+def my_extractor(preprocessed_data, layers_to_extract, model):
+    # Create a extractor instance
+    extractor_model = tx.Extractor(model, layers_to_extract)
+
+    # Extract actual features
+    _, features = extractor_model(preprocessed_data)
+
+    return features
+
+
+def my_cleaner(features):
+    return features
+
+
+if __name__ == '__main__':
+    num = int(sys.argv[1])
+    standard = ['ResNet50', 'AlexNet', 'Densenet121', 'Densenet161', 'Densenet169',
+                'Densenet201', 'GoogleNet', 'ResNet101', 'ResNet152', 'ResNet18',
+                'ResNet34', 'ShuffleNetV2x05', 'ShuffleNetV2x10', 'Squeezenet1_0', 'Squeezenet1_1',
+                'VGG11', 'VGG11_bn', 'VGG13', 'VGG13_bn', 'VGG16',
+                'VGG16_bn', 'VGG19', 'VGG19_bn', 'efficientnet_b0', 'efficientnet_b1',
+                'efficientnet_b2', 'efficientnet_b3', 'efficientnet_b4', 'efficientnet_b5', 'mnasnet05',
+                'mnasnet10', 'mobilenet_v2', 'mobilenet_v3_large', 'mobilenet_v3_small']
+    timm = ['inception_v3', 'inception_resnet_v2', 'xception', 'tf_efficientnet_b2_ns', 'tf_efficientnet_b4_ns',
+            'resnext50_32x4d', 'resnext101_32x8d', 'vit_base_patch16_224', 'vit_large_patch16_224', 'deit_base_patch16_224',
+            'swin_base_patch4_window7_224', 'mixer_b16_224', 'nfnet_l0', 'dm_nfnet_f0', 'regnety_032',
+            'regnety_080', 'coat_lite_mini','seresnet50', 'gluon_resnet50_v1c', 'gluon_resnext101_64x4d',
+            'wide_resnet50_2', 'convit_small']
+    timm_custom = ['efficientnet_b3.ra2_in1k', 'beit_base_patch16_224.in22k_ft_in22k_in1k', 'gmlp_s16_224.ra3_in1k', 'convnext_base.fb_in22k_ft_in1k']
+    models_list = ['ResNet50', 'AlexNet', 'Densenet121', 'Densenet161', 'Densenet169',
+                   'Densenet201', 'GoogleNet', 'ResNet101', 'ResNet152', 'ResNet18',
+                   'ResNet34', 'ShuffleNetV2x05', 'ShuffleNetV2x10', 'Squeezenet1_0', 'Squeezenet1_1',
+                   'VGG11', 'VGG11_bn', 'VGG13', 'VGG13_bn', 'VGG16',
+                   'VGG16_bn', 'VGG19', 'VGG19_bn', 'efficientnet_b0', 'efficientnet_b1',
+                   'efficientnet_b2', 'efficientnet_b3', 'efficientnet_b4', 'efficientnet_b5', 'mnasnet05',
+                   'mnasnet10', 'mobilenet_v2', 'mobilenet_v3_large', 'mobilenet_v3_small',
+                   'inception_v3', 'inception_resnet_v2', 'xception', 'tf_efficientnet_b2_ns', 'tf_efficientnet_b4_ns',
+                   'resnext50_32x4d', 'resnext101_32x8d', 'vit_base_patch16_224', 'vit_large_patch16_224',
+                   'deit_base_patch16_224',
+                   'swin_base_patch4_window7_224', 'mixer_b16_224', 'nfnet_l0', 'dm_nfnet_f0', 'regnety_032',
+                   'regnety_080', 'coat_lite_mini', 'seresnet50', 'gluon_resnet50_v1c', 'gluon_resnext101_64x4d',
+                   'wide_resnet50_2', 'convit_small',
+                   'inception_v3', 'inception_resnet_v2', 'xception', 'tf_efficientnet_b2_ns', 'tf_efficientnet_b4_ns',
+                   'resnext50_32x4d', 'resnext101_32x8d', 'vit_base_patch16_224', 'vit_large_patch16_224',
+                   'deit_base_patch16_224',
+                   'swin_base_patch4_window7_224', 'mixer_b16_224', 'nfnet_l0', 'dm_nfnet_f0', 'regnety_032',
+                   'regnety_080', 'coat_lite_mini', 'seresnet50', 'gluon_resnet50_v1c', 'gluon_resnext101_64x4d',
+                   'wide_resnet50_2', 'convit_small']
+
+    model_name = models_list[num]
+
+    stimuli_path = os.path.join(os.getcwd(), "Illusion_images")
+    fmri_dataset = "fmri_illusion"
+    save_folder = "encoding_illusion"
+    if num < len(standard):
+        features = encoding(model_name, "V1", "Standard", stimuli_path, fmri_dataset, save_folder, 6)
+        encoding(model_name, "V2", "Standard", stimuli_path, fmri_dataset, save_folder, 6, features)
+        encoding(model_name, "V4", "Standard", stimuli_path, fmri_dataset, save_folder, 6, features)
+        encoding(model_name, "IT", "Standard", stimuli_path, fmri_dataset, save_folder, 6, features)
+    elif num < len(timm):
+        features = encoding(model_name,"Timm", "V1", stimuli_path, fmri_dataset, save_folder, 6)
+        encoding(model_name, "V2", "Timm", stimuli_path, fmri_dataset, save_folder, 6, features)
+        encoding(model_name, "V4", "Timm", stimuli_path, fmri_dataset, save_folder, 6, features)
+        encoding(model_name, "IT", "Timm", stimuli_path, fmri_dataset, save_folder, 6, features)
+    else:
+        features = encoding_custom(model_name,  "V1", stimuli_path, fmri_dataset, save_folder, 6)
+        encoding_custom(model_name, "V2", stimuli_path, fmri_dataset, save_folder, 6, features)
+        encoding_custom(model_name, "V4", stimuli_path, fmri_dataset, save_folder, 6, features)
+        encoding_custom(model_name, "IT", stimuli_path, fmri_dataset, save_folder, 6, features)
