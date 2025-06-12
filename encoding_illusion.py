22c96421c3e7d2d18dcf6e5497d23a69c0424bc2
TrungDavidNguyen <davidtrung03@gmail.com>
2025-06-12 23:55:01 +0200

    fix

--- encoding_illusion.py
+++ encoding_illusion.py
@@ -104,7 +104,7 @@ def my_cleaner(features):
 
 
 if __name__ == '__main__':
-    num = int(sys.argv[1])
+    num = 1
     standard = ['ResNet50', 'AlexNet', 'Densenet121', 'Densenet161', 'Densenet169',
                 'Densenet201', 'GoogleNet', 'ResNet101', 'ResNet152', 'ResNet18',
                 'ResNet34', 'ShuffleNetV2x05', 'ShuffleNetV2x10', 'Squeezenet1_0', 'Squeezenet1_1',
