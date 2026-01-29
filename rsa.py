8f0399790b7fc886883fc685866c1f1bd2f22f46
TrungDavidNguyen <davidtrung03@gmail.com>
2026-01-29 22:16:26 +0100

    no longer square spearman

--- rsa.py
+++ rsa.py
@@ -90,15 +90,8 @@ def my_cleaner(features):
 
 if __name__ == '__main__':
     num = int(sys.argv[1])
-    standard = ['ResNet50', 'AlexNet', 'Densenet121', 'Densenet161', 'Densenet169',
-                'Densenet201', 'GoogleNet', 'ResNet101', 'ResNet152', 'ResNet18',
-                'ResNet34', 'ShuffleNetV2x05', 'ShuffleNetV2x10', 'Squeezenet1_0', 'Squeezenet1_1',
-                'VGG11', 'VGG11_bn', 'VGG13', 'VGG13_bn', 'VGG16',
-                'VGG16_bn', 'VGG19', 'VGG19_bn', 'efficientnet_b0', 'efficientnet_b1',
-                'efficientnet_b2', 'efficientnet_b3', 'efficientnet_b4', 'efficientnet_b5', 'mnasnet05',
-                'mnasnet10', 'mobilenet_v2', 'mobilenet_v3_large', 'mobilenet_v3_small']
-    timm = ['inception_v3', 'inception_resnet_v2', 'xception', 'tf_efficientnet_b2_ns', 'tf_efficientnet_b4_ns',
-            'resnext50_32x4d', 'resnext101_32x8d', 'vit_base_patch16_224', 'vit_large_patch16_224',
+    standard = []
+    timm = ['tf_efficientnet_b4_ns', 'resnext50_32x4d', 'resnext101_32x8d', 'vit_base_patch16_224', 'vit_large_patch16_224',
             'deit_base_patch16_224',
             'swin_base_patch4_window7_224', 'mixer_b16_224', 'nfnet_l0', 'dm_nfnet_f0', 'regnety_032',
             'regnety_080', 'coat_lite_mini', 'seresnet50', 'gluon_resnet50_v1c', 'gluon_resnext101_64x4d',
